a41a0fd8aed4e900af90a8eb446faa464acedc24
Simone Fonda <fonda@spaziodati.eu>
2015-12-02 11:12:21 +0100

    Chrome extension version 1

--- extension/app/js/app.js
+++ extension/app/js/app.js
@@ -1,1 +1,30 @@
-alert('omg');
+chrome.browserAction.onClicked.addListener(function() {
+	console.log('on clicked listener', arguments);
+	alert(1);
+});
+
+// When the page is activated (or the tab is getting the focus)
+chrome.tabs.onActivated.addListener(function() {
+	console.log('on activated', arguments);
+	alert(window.location);
+	$(document).prepend('<div>omgomgomg</div>')
+});
+
+chrome.tabs.onUpdated.addListener(function() {
+	console.log('on updated', arguments);
+	alert(3);
+
+});
+
+
+// Urls is replaced with something else
+chrome.tabs.onReplaced.addListener(function() {
+	console.log('on replaced', arguments);
+	alert(5);
+
+});
+
+//chrome.tabs.executeScript(tabId, {
+//    file: 'app/page_runtime.js',
+//    runAt: 'document_start'
+//});
